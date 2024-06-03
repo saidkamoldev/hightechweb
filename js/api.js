52b51cdf905c719d838696e8fbcf492bfab3b18d
saidkamoldev <juraqulovsaidkamol@gmail.com>
2024-06-04 01:26:01 +0500

    full-project

--- js/api.js
+++ js/api.js
@@ -1,3 +1,46 @@
+// document.getElementById('clientForm').addEventListener('submit', function(event) {
+//   event.preventDefault();
+
+//   let name = document.getElementById('name').value;
+//   let phone_number = document.getElementById('phone_number').value;
+
+//   fetch('https://admin.kosmostj.uz/client/', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//       'X-CSRFToken': getCookie('csrftoken')  // CSRF tokenini oling
+//     },
+//     body: JSON.stringify({
+//       name: name,
+//       phone_number: phone_number
+//     })
+//   })
+//   .then(response => response.json())
+//   .then(data => {
+//     console.log('Success:', data);
+//   })
+//   .catch((error) => {
+//     console.error('Error:', error);
+//   });
+// });
+
+// function getCookie(name) {
+//   let cookieValue = null;
+//   if (document.cookie && document.cookie !== '') {
+//     let cookies = document.cookie.split(';');
+//     for (let i = 0; i < cookies.length; i++) {
+//       let cookie = cookies[i].trim();
+//       // Bu cookie name bilan boshlanyaptimi?
+//       if (cookie.substring(0, name.length + 1) === (name + '=')) {
+//         cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+//         break;
+//       }
+//     }
+//   }
+//   return cookieValue;
+// }
+
+
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('.contact-form button').addEventListener('click', function() {
         var name = document.querySelector('.contact-form input[type="text"]').value;
@@ -9,7 +52,7 @@ document.addEventListener('DOMContentLoaded', function() {
         };
 
         var xhr = new XMLHttpRequest();
-        xhr.open('POST', 'http://127.0.0.1:8000/client/', true);
+        xhr.open('POST', 'https://admin.kosmostj.uz/client/', true);
         xhr.setRequestHeader('Content-Type', 'application/json');
         xhr.onload = function() {
             if (xhr.status === 200) {
